91814b1c3ccfd4b3e54eee049be314138c723c35
Håkan <hali0151@student.umu.se>
2022-04-20 18:22:22 +0200

    Redo the layout
    Add hover effects and zoom effects, need to decide which way to go

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -1,23 +1,41 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Project({ project }) {
+  const [hover, setHover] = useState(false);
+
   return (
     <Link href={`/project/${project.slug}`} passHref>
-      <div className="flex flex-col max-w-sm max-h-sm overflow-hidden shadow-lg bg-primary dark:bg-primary-dark
-                    text-secondary 
-                      h-full cursor-pointer border-b-4 border-accent-dark">
-        <img
-          className="w-full"
-          src={project.frontmatter.cover_image}
-          alt="Project Image"
-        />
-        <div className="px-6 py-4">
+      <div
+        className="flex flex-col overflow-hidden shadow-lg bg-primary dark:bg-primary-dark
+                    text-secondary relative
+                      h-full cursor-pointer"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div className="relative overflow-hidden m-w-full w-full group">
+          <img
+            className="m-w-full w-full group-hover:scale-125 group-hover:skew-x-12"
+            src={project.frontmatter.cover_image}
+            alt="Project Image"
+          />
+        </div>
+        {hover ? (
+          <div
+            className="bg-overlay absolute top-1/2 right-1/2 flex 
+                            flex-col items-center justify-center"
+          >
+            <p className="text-3xl font-bold text-accent bg-primary-dark p-4 rounded-xl">
+              {project.frontmatter.title}
+            </p>
+          </div>
+        ) : null}
+        {/* <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2 text-teal-600">
             {project.frontmatter.title}
           </div>
           <p className="text-base dark:text-secondary-dark">{project.frontmatter.excerpt}</p>
-        </div>
+        </div> */}
         {/* <div className="px-6 py-4">
           <div className="font-bold text-md mb-2 text-red-300">
             Created {project.frontmatter.date}
