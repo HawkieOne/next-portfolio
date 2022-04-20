89a8e8ac997faf50e77cd8b0e4ce38f64e86862a
Håkan <hali0151@student.umu.se>
2022-04-20 18:24:24 +0200

    Removed mouse effects and add button download CV

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import WelcomeText from "../components/Home/WelcomeText";
+// import CV from "../assets/CV.pdf";
 
 const IndexPage = () => {
-  var dots = [],
-    mouse = {
-      x: 0,
-      y: 0,
-    };
-
-  // useEffect(() => {
-  //   var Dot = function () {
-  //     this.x = 0;
-  //     this.y = 0;
-  //     if (document) {
-  //       this.node = (function () {
-  //         var n = document.createElement("div");
-  //         n.className = "trail";
-  //         document.body.appendChild(n);
-  //         return n;
-  //       })();
-  //     }
-  //   };
-  //   Dot.prototype.draw = function () {
-  //     this.node.style.left = this.x + "px";
-  //     this.node.style.top = this.y + "px";
-  //   };
-  //   for (var i = 0; i < 24; i++) {
-  //     var d = new Dot();
-  //     dots.push(d);
-  //   }
-
-  //   function draw() {
-  //     var x = mouse.x + 100,
-  //       y = mouse.y + 10;
-
-  //     // This loop is where all the 90s magic happens
-  //     dots.forEach(function (dot, index, dots) {
-  //       var nextDot = dots[index + 1] || dots[0];
-
-  //       dot.x = x;
-  //       dot.y = y;
-  //       dot.draw();
-  //       x += (nextDot.x - dot.x) * 0.6;
-  //       y += (nextDot.y - dot.y) * 0.6;
-  //     });
-  //   }
-
-  //   function animate() {
-  //     draw();
-  //     requestAnimationFrame(animate);
-  //   }
-  //   animate();
-  // }, []);
-
-  useEffect(() => {
-    // const moveCursor = (e) => {
-    //   mouse.x = e.pageX;
-    //   mouse.y = e.pageY;
-    // };
-    // window.addEventListener("mousemove", moveCursor);
-    // return () => {
-    //   window.removeEventListener("mousemove", moveCursor);
-    // };
-  }, []);
 
   const dirsX = [
     [-200, 0],
@@ -82,8 +22,35 @@ const IndexPage = () => {
   const randomXSkew = skewX[Math.floor(Math.random() * skewX.length)];
 
   return (
-    <motion.div className="h-full flex items-center justify-center">
-      {/* <div className="max-h-1/2 w-1/3 cursor-none relative"> */}
+    <motion.div className="h-full flex flex-col items-center justify-center relative">
+      <div
+        className="flex items-center justify-center absolute top-6 right-6 p-4 
+                      space-x-2"
+      >
+        <motion.div
+          animate={{ x: [-30, 0] }}
+          transition={{
+            ease: "anticipate",
+            repeat: Infinity,
+            repeatType: "mirror",
+            duration: 0.5,
+            repeatDelay: 1,
+          }}
+          className="relative"
+        >
+          <img src={"/svg/send.svg"} />
+        </motion.div>
+        <a
+          // href={CV}
+          title=""
+          download
+          className="p-4 shadow-lg rounded-md cursor-pointer
+                  border border-secondary dark:border-accent-dark"
+        >
+          Download CV
+        </a>
+      </div>
+
       <div className="h-screen relative flex items-end">
         {/* <Canvas draw={draw} options={{}} /> */}
         <motion.div
@@ -93,16 +60,16 @@ const IndexPage = () => {
             repeat: Infinity,
             repeatType: "mirror",
             duration: 1,
-            repeatDelay: 4
+            repeatDelay: 4,
           }}
-          className="basis-1/2 h-3/4 bg-primary dark:bg-primary-dark"          
+          className="basis-1/2 h-3/4 bg-primary dark:bg-primary-dark"
         >
           <img
             src="images/drawing.png"
             alt="drawing of me"
             className="mix-blend-multiply dark:mix-blend-luminosity h-full
-                      slidingImg"            
-          />
+                      slidingImg"
+          />        
         </motion.div>
         <div className="self-center">
           <WelcomeText />
