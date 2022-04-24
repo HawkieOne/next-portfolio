72c84e4a4a382e12be15c1ffc45fd568191bceb8
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-04-24 13:46:56 +0200

    Change name

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://hakanlindahl.com", "http://localhost:3000"],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
@@ -23,11 +23,11 @@ var mail = nodemailer.createTransport({
   },
 });
 
-app.get("/server", (req, res) => {
-  res.send(`Hello there`);
+app.get("/serverDrawing", (req, res) => {
+  res.send(`GET: Hello there`);
 });
 
-app.post("/server", jsonParser, (req, res) => {
+app.post("/serverDrawing", jsonParser, (req, res) => {
   var mailOptions = {
     from: "lindahl9898@gmail.com",
     to: "lindahl9898@gmail.com",
@@ -39,18 +39,53 @@ app.post("/server", jsonParser, (req, res) => {
         filename: "drawing.jpg",
         path: req.body.content,
         cid: "drawing",
-        encoding: 'base64'
+        encoding: "base64",
       },
     ],
   };
   mail.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.log(error);
+      return res.status(400).send({
+        message: "The mail could not be sent!",
+      });
     } else {
       console.log("Email sent: " + info.response);
+      res.send(`Mail sent`);
     }
-  });
-  res.send(`Mail sent`);
+  });  
+});
+
+app.post("/serverContact", jsonParser, (req, res) => {
+  var mailOptions = {
+    from: "lindahl9898@gmail.com",
+    to: "lindahl9898@gmail.com",
+    subject: "Contact from portfolio",
+    text:
+      "Name: " +
+      req.body.from_name +
+      "\n" +
+      "Mail: " +
+      req.body.from_email +
+      "\n" +
+      "Message: " +
+      req.body.message +
+      "\n" +
+      "Rating: " +
+      req.body.rating +
+      "\n",
+  };
+  console.log(req.body.rating);
+  mail.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      return res.status(400).send({
+        message: "The mail could not be sent!",
+      });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send(`Mail sent`);
+    }
+  });  
 });
 
 app.listen(9000, function () {
