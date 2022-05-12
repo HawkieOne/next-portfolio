8a0969d9f51edc4cb272e73e6d1f22be080c7daa
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-05-12 17:00:03 +0200

    remove unused atoms
    - avoids errors from double atoms (Fatal error)

--- atoms/atoms.ts
+++ atoms/atoms.ts
@@ -1,20 +1,5 @@
 import { atom } from "recoil";
 
-export const currentLink = atom({
-    key: "currentLink",
-    default: '/',
-});
-
-export const hoverSlidingImg = atom({
-    key: "hoverSlidingImg",
-    default: false,
-});
-
-export const menuOpen = atom({
-    key: "menuOpen",
-    default: false,
-});
-
 export const currentProjectIndex = atom({
     key: "currentProjectIndex",
     default: 1,
